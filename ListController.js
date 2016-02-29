cab27a5397987a52d8253382659597e317542cea
Daniel Cain <d@deecio.com>
2016-02-28 20:54:53 -0600

    Added controller file
    Added the .js file for the AngularJS app

---
+++ ListController.js
@@ -0,0 +1,19 @@
+﻿app.controller('ListController', ['$scope', function ($scope) {
+    $scope.LEDLamp = {
+        icon: '',
+        name: 'Desk Lamp: LED Bulb',
+        price: 6.99
+    };
+
+    $scope.HaloLamp = {
+        icon: '',
+        name: 'Desk Lamp: Halogen Bulb',
+        price: 0.99
+    };
+
+    $scope.ESMicrowave = {
+        icon: '',
+        name: 'Microwave: Energy Star Approved',
+        price: 199.99
+    };
+}]);
