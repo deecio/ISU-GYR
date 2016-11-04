5879ba1e6d9181b6cf78bb2089f10aa02d50449a
deecio <d@deecio.com>
2016-11-04 13:24:39 -0500

    Fixed card images in SC, added new formatting (new categories list and all-around touch-ups), made fade-ins longer to better mark screen transitions, fixed screen order for SC

--- www/ShoppingCartPage/app.js
+++ www/ShoppingCartPage/app.js
@@ -11,7 +11,7 @@ $(document).ready(function () {
 function switchShopScreens(screenClass){
     $(".shop-screen").hide();
 
-    $(screenClass).fadeIn();
+    $(screenClass).fadeIn(800);
 };
 
 function showSidebar() {
