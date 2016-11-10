599c97df90c7084404a4c330fd0e1fd4d5acba82
Brianna Gerads <brianna.gerads@gmail.com>
2016-11-10 01:10:17 -0600

    add savingscart admin pages and database access

--- www/admin/backend/adminbackend.js
+++ www/admin/backend/adminbackend.js
@@ -1,6 +1,7 @@
 var database = require('./database');
 module.exports = {};
 
+//Quiz
 module.exports.populatequiz = function(req, res){
 var rtn = {};
         function populatequiz(err, result, fields) {
@@ -54,6 +55,32 @@ var rtn = {};
 
 };
 
+//Shopping Cart
+module.exports.viewshoppingcart = function(req,res){
+  var rtn = {};
+          function addfact(err, result, fields) {
+              rtn.db_result = result;
+              rtn.ready = true;
+              res.json(rtn);
+          }
+          database.query('INSERT INTO `item`(`name`, `icon`, `icon_height`, `monthly_cost`, `eStar`, `object`, `type`) VALUES (?,?,?,?,?,?,?,?);', [req.body.name || "null", req.body.icon || "null", req.body.icon_height || "null", req.body.monthly_cost || "null", req.body.eStar || "null", req.body.object || "null", req.body.type || "null"], viewshoppingcart);
+
+};
+
+module.exports.getshoppingcart = function(req,res){
+  var rtn = {};
+          function getshoppingcart(err, result, fields) {
+              rtn.db_result = result;
+              if (err)
+                  rtn.error = err;
+              rtn.ready = true;
+              res.json(rtn);
+          }
+          database.query('SELECT `name`, `icon`, `icon_height`, `monthly_cost`, `object`, `type`, `eStar` FROM `item`;', getshoppingcart);
+
+};
+
+//Information
 module.exports.getinformation = function(req,res){
   var rtn = {};
           function getinformation(err, result, fields) {
@@ -67,6 +94,27 @@ module.exports.getinformation = function(req,res){
 
 };
 
+module.exports.addinformation = function(req,res){
+  var rtn = {};
+          function addfact(err, result, fields) {
+              rtn.db_result = result;
+              if (err)
+                  rtn.error = err;
+                  if (result.changedRows < 1) {
+                      rtn.error = {
+                          code: 1001,
+                          message: 'failed to add information'
+                      }
+                      //console.log("/vote/" + uid + "/" + dir + " - ", result);
+                  }
+              rtn.ready = true;
+              res.json(rtn);
+          }
+          database.query('INSERT INTO `fact`(`name`, `text`, `category`, `hyperlink`, `image`) VALUES (?,?,?,?,?);', [req.body.name || "null", req.body.text || "null", req.body.category || "null", req.body.hyperlink || "null", req.body.image || "null"], addinformation);
+
+};
+
+//Facts
 module.exports.getfacts = function(req,res){
   var rtn = {};
           function getfacts(err, result, fields) {
@@ -89,7 +137,7 @@ module.exports.addfact = function(req,res){
                   if (result.changedRows < 1) {
                       rtn.error = {
                           code: 1001,
-                          message: 'failed to retrieve fact information'
+                          message: 'failed to add fact information'
                       }
                       //console.log("/vote/" + uid + "/" + dir + " - ", result);
                   }
--- www/admin/backend/app.js
+++ www/admin/backend/app.js
@@ -3,6 +3,7 @@ var app = express();
 var database = require('./database');
 var fs = require('fs');
 var adminbackend = require('./adminbackend');
+var bodyParser = require('body-parser');
 
 var allowCrossDomain = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', 'http://greenyourresidence.ece.iastate.edu');
@@ -12,6 +13,13 @@ var allowCrossDomain = function(req, res, next) {
     next();
 }
 app.use(allowCrossDomain);
+
+app.use(bodyParser.json({
+  type: 'application/json'
+}));
+app.use(bodyParser.text({
+  type: 'text/plain'
+}));
 
 // Test Case: If server is running, it will print Hello World
 app.get('/', function (req, res) {
@@ -34,6 +42,7 @@ app.get('/QuizController', function(req, res){
 //View Quizzes
 app.get('/QuestionViewerDB', adminbackend.showquizzes);
 app.get('/QuizViewerDB', adminbackend.getquizzes);
+app.get('/AddQuestionDB', adminbackend.addquestionDB);
 
 app.get('/QuizViewer', function(req, res){
   console.log('QuizViewer')
@@ -44,9 +53,13 @@ app.get('/QuizViewer', function(req, res){
   res.json(rtn);
 });
 
-//View Information
-app.get('/InformationViewerDB', adminbackend.getinformation);
+//Shopping Cart
+app.get('ShoppingCartViewerDB', adminbackend.getshoppingcart);
+app.get('AddItemDB', adminbackend.getshoppingcart);
 
+//Information
+app.get('/InformationViewerDB', adminbackend.getinformation);
+app.post('/AddInformationDB', adminbackend.addinformation);
 app.get('/InformationViewer', function(req, res){
   console.log('InformationViewer')
   var rtn = {};//init an object to return
@@ -61,5 +74,5 @@ app.listen(3000, function () {
 });
 
 //Facts
-app.get('/AddFactDB', adminbackend.addfact);
+app.post('/AddFactDB', adminbackend.addfact);
 app.get('/FactViewerDB', adminbackend.getfacts);
