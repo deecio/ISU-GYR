494bf832897e31c2a5ea489e2c4bffe57015b5a2
Brianna Gerads <brianna.gerads@gmail.com>
2016-12-05 19:57:26 -0600

    add delete to backend

--- www/admin/backend/adminbackend.js
+++ www/admin/backend/adminbackend.js
@@ -34,8 +34,28 @@ module.exports.gethomeitems = function(req,res){
 
 };
 
+module.exports.deletehomeitem = function(req,res){
+  var rtn = {};
+          function deletehomeitem(err, result, fields) {
+              rtn.db_result = result;
+              if (err)
+                  rtn.error = err;
+                  if (result.changedRows < 1) {
+                      rtn.error = {
+                          code: 1001,
+                          message: 'failed to delete question'
+                      }
+                  }
+              rtn.ready = true;
+              res.json(rtn);
+          }
+          database.query('DELETE FROM `object` WHERE `tooltip` =VALUES (?);', [req.body.tooltip], deletehomeitem);
+
+};
 
 //Quiz
+
+// Not used
 module.exports.populatequiz = function(req, res){
 var rtn = {};
         function populatequiz(err, result, fields) {
@@ -58,21 +78,22 @@ var rtn = {};
 };
 
 
-module.exports.getquizzes = function(req, res){
+module.exports.getquestions = function(req, res){
 var rtn = {};
-        function getquizzes(err, result, fields) {
+        function getquestions(err, result, fields) {
             rtn.db_result = result;
             if (err)
                 rtn.error = err;
             rtn.ready = true;
             res.json(rtn);
         }
-        database.query('SELECT * FROM `quiz`;', getquizzes);
+        database.query('SELECT * FROM `question` ORDER BY `question`.`category` ASC;', getquestions);
 
 
 
 };
 
+// Not used
 module.exports.showquizzes = function(req, res){
 var rtn = {};
         function showquizzes(err, result, fields) {
@@ -105,13 +126,34 @@ module.exports.addquestionDB = function(req,res){
 
 };
 
-//Shopping Cart
-module.exports.additem = function(req,res){
+module.exports.deletequestion = function(req,res){
   var rtn = {};
-          function additem(err, result, fields) {
+          function deletequestion(err, result, fields) {
               rtn.db_result = result;
               if (err)
                   rtn.error = err;
+                  if (result.changedRows < 1) {
+                      rtn.error = {
+                          code: 1001,
+                          message: 'failed to delete question'
+                      }
+                  }
+              rtn.ready = true;
+              res.json(rtn);
+          }
+          database.query('DELETE FROM `question` WHERE `label` =VALUES (?);', [req.body.label], deletequestion);
+
+};
+
+//Shopping Cart
+module.exports.additem = function(req,res){
+      var rtn = {};
+          function additem(err, result, fields) {
+              rtn.db_result = result;
+              if (err){
+                  rtn.error = err;
+                  console.log(err);
+                }
                   if (result.changedRows < 1) {
                       rtn.error = {
                           code: 1001,
@@ -135,6 +177,25 @@ module.exports.getshoppingcart = function(req,res){
               res.json(rtn);
           }
           database.query('SELECT * FROM `item`;', getshoppingcart);
+
+};
+
+module.exports.deleteitem = function(req,res){
+  var rtn = {};
+          function deleteitem(err, result, fields) {
+              rtn.db_result = result;
+              if (err)
+                  rtn.error = err;
+                  if (result.changedRows < 1) {
+                      rtn.error = {
+                          code: 1001,
+                          message: 'failed to delete item'
+                      }
+                  }
+              rtn.ready = true;
+              res.json(rtn);
+          }
+          database.query('DELETE FROM `item` WHERE `name` = VALUES (?);', [req.body.name], deleteitem);
 
 };
 
@@ -173,6 +234,25 @@ module.exports.addinformation = function(req,res){
 
 };
 
+module.exports.deleteinformation = function(req,res){
+  var rtn = {};
+          function deleteinformation(err, result, fields) {
+              rtn.db_result = result;
+              if (err)
+                  rtn.error = err;
+                  if (result.changedRows < 1) {
+                      rtn.error = {
+                          code: 1001,
+                          message: 'failed to delete information'
+                      }
+                  }
+              rtn.ready = true;
+              res.json(rtn);
+          }
+          database.query('DELETE FROM `information` WHERE `title` = VALUES (?);', [req.body.title], deleteinformation);
+
+};
+
 //Facts
 module.exports.getfacts = function(req,res){
   var rtn = {};
@@ -203,5 +283,24 @@ module.exports.addfact = function(req,res){
               res.json(rtn);
           }
           database.query('INSERT INTO `fact`(`name`, `text`, `category`, `hyperlink`, `image`) VALUES (?,?,?,?,?);', [req.body.name || "null", req.body.text || "null", req.body.category || "null", req.body.hyperlink || "null", req.body.image || "null"], addfact);
+
+};
+
+module.exports.deletefact = function(req,res){
+  var rtn = {};
+          function deletefact(err, result, fields) {
+              rtn.db_result = result;
+              if (err)
+                  rtn.error = err;
+                  if (result.changedRows < 1) {
+                      rtn.error = {
+                          code: 1001,
+                          message: 'failed to delete fact'
+                      }
+                  }
+              rtn.ready = true;
+              res.json(rtn);
+          }
+          database.query('DELETE FROM `fact` WHERE `text` = VALUES (?);', [req.body.text], deletefact);
 
 };
