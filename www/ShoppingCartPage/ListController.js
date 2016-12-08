7a3400ef72e747299fa488e3479bc211d4e4f65e
deecio <d@deecio.com>
2016-12-07 23:23:27 -0600

    Added new content for the shopping cart module

--- www/ShoppingCartPage/ListController.js
+++ www/ShoppingCartPage/ListController.js
@@ -10,106 +10,170 @@
 
     $scope.items = [
         {
-            icon: '../img/ledbulb.jpg',
-            icon_height: "150px",
-            name: 'Desk Lamp: LED Bulb',
-            monthly_cost: 10,
-            object: 'lighting',
-            type: 'lamp',
-            eStar: true
+            icon: '../img/SC/LED Bulb.png',
+            name: 'LED Bulb',
+            monthly_cost: 0.77,
+            object: 'lighting'
         },
 
         $scope.FluorLamp = {
-            icon: '../img/fluorbulb.jpg',
-            icon_height: "150px",
-            name: 'Desk Lamp: Fluorescent Bulb',
-            monthly_cost: 30,
-            object: 'lighting',
-            type: 'lamp',
-            eStar: true
+            icon: '../img/SC/CFL Bulb.png',
+            name: 'CFL Bulb',
+            monthly_cost: 1.54,
+            object: 'lighting'
         },
 
         $scope.IncanLamp = {
-            icon: '../img/IncanBulb.png',
-            icon_height: '150px',
-            name: 'Desk Lamp: Incandescent Bulb',
-            monthly_cost: 50,
-            object: 'lighting',
-            type: 'lamp',
-            eStar: false
+            icon: '../img/SC/Incandecesant Bulb.png',
+            name: 'Incandescent Bulb',
+            monthly_cost: 6.60,
+            object: 'lighting'
         },
 
-        $scope.ESMicrowave = {
-            icon: '../img/microwave.jpg',
-            icon_height: "150px",
-            name: 'Microwave: Energy Star Approved',
-            monthly_cost: 20,
-            object: 'appliance',
-            type: 'microwave',
-            eStar: true
+        {
+            icon: '../img/SC/Mini Fridge Black.png',
+            name: 'Mini-Fridge (no freezer)',
+            monthly_cost: 66,
+            object: 'refrigerator'
         },
 
-        $scope.RegularMicrowave = {
-            icon: '../img/microwave.jpg',
-            icon_height: "150px",
-            name: 'Microwave: Standard',
-            monthly_cost: 50,
-            object: 'appliance',
-            type: 'microwave',
-            eStar: false
+        {
+            icon: '../img/SC/Mini Fridge Silver.png',
+            name: 'Micro-Fridge',
+            monthly_cost: 77,
+            type: 'refrigerator'
         },
 
-        $scope.Refrigerator = {
-            icon: '../img/miniFridge.jpg',
-            icon_height: '150px',
-            name: 'Refrigerator: Stock, Compact',
-            monthly_cost: 40,
-            object: 'appliance',
-            type: 'refrigerator',
-            eStar: false
+        {
+            icon: '../img/SC/Microwave Black.png',
+            name: 'Microwave',
+            monthly_cost: 165,
+            object: 'microwave'
         },
 
-        $scope.ESRefrigerator = {
-            icon: '../img/miniFridge.jpg',
-            icon_height: '150px',
-            name: 'Refrigerator: EnergyStar, Compact',
-            monthly_cost: 25,
-            object: 'appliance',
-            type: 'refrigerator',
-            eStar: true
+        {
+            icon: '../img/SC/Toaster.png',
+            name: 'Toaster',
+            monthly_cost: 115.50,
+            object: 'microwave'
         },
 
-        $scope.ESBlender = {
-            icon: '../img/blender.jpg',
-            icon_height: '150px',
-            name: 'Blender: EnergyStar Approved',
-            monthly_cost: 8,
-            object: 'appliance',
-            type: 'blender',
-            eStar: true
+        {
+            icon: '../img/SC/ToasterOven.png',
+            name: 'Toaster Oven',
+            monthly_cost: 132,
+            object: 'microwave'
         },
 
-        $scope.ESBlender = {
-            icon: '../img/blender.jpg',
-            icon_height: '150px',
-            name: 'Blender: Standard',
-            monthly_cost: 16,
-            object: 'appliance',
-            type: 'blender',
-            eStar: false
+        {
+            icon: '../img/SC/Coffee Pot Black.png',
+            name: 'Traditional (Drip) Coffee Maker',
+            monthly_cost: 132,
+            object: 'coffee'
+        },
+
+        {
+            icon: '../img/SC/Keurig Black.png',
+            name: 'Keurig Coffee Maker',
+            monthly_cost: 165,
+            object: 'coffee'
+        },
+
+        {
+            icon: '../img/SC/Wii.png',
+            name: 'Nintendo Wii',
+            monthly_cost: 2.09,
+            object: 'gaming'
+        },
+
+        {
+            icon: '../img/SC/XBOX.png',
+            name: 'Microsoft Xbox 360',
+            monthly_cost: 18.70,
+            object: 'gaming'
+        },
+
+        {
+            icon: '../img/SC/PS3.png',
+            name: 'Sony Playstation 3',
+            monthly_cost: 21.67,
+            object: 'gaming'
+        },
+
+        {
+            icon: '../img/SC/MagicBullet.png',
+            name: 'Magic Bullet',
+            monthly_cost: 27.50,
+            object: 'blender'
+        },
+
+        {
+            icon: '../img/SC/Blender.png',
+            name: 'Traditional Blender',
+            monthly_cost: 55,
+            object: 'blender'
+        },
+
+        {
+            icon: '../img/SC/NutriBullet.png',
+            name: 'NutriBullet',
+            monthly_cost: 66,
+            object: 'blender'
+        },
+
+        {
+            icon: '../img/SC/TV Flat Screen Black.png',
+            name: 'LCD Flatscreen TV',
+            monthly_cost: 30.25,
+            object: 'tv'
+        },
+
+        {
+            icon: '../img/SC/TV Plasma.png',
+            name: 'Plasma Flatscreen TV',
+            monthly_cost: 43.89,
+            object: 'tv'
+        },
+
+        {
+            icon: '../img/SC/PrinterJet.png',
+            name: 'Inkjet Printer',
+            monthly_cost: 19.80,
+            object: 'printer'
+        },
+
+        {
+            icon: '../img/SC/PrinterLaser.jpg',
+            name: 'Laser Printer',
+            monthly_cost: 88,
+            object: 'printer'
+        },
+
+        {
+            icon: '../img/SC/PC.png',
+            name: 'Desktop Computer',
+            monthly_cost: 8.25,
+            object: 'computer'
+        },
+
+        {
+            icon: '../img/SC/Laptop.png',
+            name: 'Laptop Computer',
+            monthly_cost: 2.75,
+            object: 'computer'
         }
     ];
     $scope.filters = [
         { name: 'All', filterExpr: '', icon: '../img/world.png' },
-        { name: 'Lights', filterExpr: { object: 'lighting' }, icon: '../img/fluorbulb.jpg' },
-        { name: 'Appliance', filterExpr: { object: 'appliance' }, icon: '../img/microwave.jpg' },
-        { name: 'Test 1', filterExpr: { object: 'appliance' }, icon: '../img/fluorbulb.jpg' },
-        { name: 'Test 2', filterExpr: { object: 'appliance' }, icon: '../img/miniFridge.jpg' },
-        { name: 'Test 3', filterExpr: { object: 'appliance' }, icon: '../img/blender.jpg' },
-        { name: 'Test 4', filterExpr: { object: 'appliance' }, icon: '../img/fluorbulb.jpg' },
-        { name: 'Test 5', filterExpr: { object: 'appliance' }, icon: '../img/miniFridge.jpg' },
-        { name: 'Test 6', filterExpr: { object: 'appliance' }, icon: '../img/blender.jpg' }
-
+        { name: 'Lighting', filterExpr: { object: 'lighting' }, icon: '../img/SC/CFL Bulb.png' },
+        { name: 'Refrigerators', filterExpr: { object: 'refrigerator' }, icon: '../img/SC/Mini Fridge Silver.png' },
+        { name: 'Toasters', filterExpr: { object: 'microwave' }, icon: '../img/SC/Microwave Black.png' },
+        { name: 'Coffee Makers', filterExpr: { object: 'coffee' }, icon: '../img/SC/Coffee Pot Black.png' },
+        { name: 'Gaming Consoles', filterExpr: { object: 'gaming' }, icon: '../img/SC/XBOX.png' },
+        { name: 'Blenders', filterExpr: { object: 'blender' }, icon: '../img/SC/Blender.png' },
+        { name: 'TVs', filterExpr: { object: 'tv' }, icon: '../img/SC/TV Plasma.png' },
+        { name: 'Printers', filterExpr: { object: 'printer' }, icon: '../img/SC/PrinterJet.png' },
+        { name: 'Computers', filterExpr: { object: 'computer' }, icon: '../img/SC/PC.png' },
         // Commenting this part out for now, bring back when screen 3 filters are changed to match the item, not the filter from screen 1.
         //{ name: 'Energy Star Approved', filterExpr: { eStar: true } }
     ];
